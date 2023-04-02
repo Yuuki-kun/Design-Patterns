6a1081314b159e62e01b77b2ea8a82bd56430a81
Yuuki-kun <tongcongminhart@gmail.com>
2023-04-02 19:04:14 +0700

    add new singleton pattern - database

---
+++ src/main/java/singleton/database/Application.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singleton.database;
+
+import java.sql.DatabaseMetaData;
+
+/**
+ *
+ * @author tongcongminh
+ */
+public class Application {
+    public static void main(String[] args) {
+        
+        Database db = Database.getInstance();
+        db.querySQL("select * from...");
+        
+        Database db2 = Database.getInstance();
+        db2.querySQL("SELECT * FROM ....");
+        
+        //the db and db1 contain the same object
+        System.out.println(db.equals(db2));
+        
+    }
+}
---
+++ src/main/java/singleton/database/Database.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singleton.database;
+
+/**
+ *
+ * @author tongcongminh
+ */
+public class Database {
+
+    private static Database instance;
+
+    private Database() {
+        //some code such as the actual connection to a database server or server socket
+        System.out.println("Database private constructor!");
+    }
+    
+    public static Database getInstance(){
+        if(Database.instance==null){
+            //object that provides the intrinsic lock
+            synchronized (Database.class) {
+                if(Database.instance==null){
+                    Database.instance = new Database();
+                }
+            }
+        }
+        return Database.instance;
+    }
+    
+    public void querySQL(String sqlSrt){
+        System.out.println("query: "+sqlSrt);
+    }
+}
